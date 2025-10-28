4c7dffb45e41e110b95f6ccd86fb2d30ad9a6082
mebi-zone <mebizone@gmail.com>
2025-10-28 10:46:03 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,12 +17,6 @@ export default function Home() {
               <span className="block">Quản lý giải đấu & </span>
               <span className="block">Phân hạng người chơi</span>
             </h1>*/}
-            /*<p className="mt-3 text-xl text-charcoal/80 text-earth">
-              Dễ dàng — Chuẩn xác — Minh bạch 
-            </p>
-            <p className="mt-3 text-xl text-charcoal/80 text-earth">
-              Đa dạng tính năng trong một hệ thống duy nhất
-            </p>*/
             <div className="text-center space-y-1">
               <p className="text-xl text-charcoal/80 text-earth">
                 Dễ dàng — Chuẩn xác — Minh bạch
